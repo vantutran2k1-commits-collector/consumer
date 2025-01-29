b4df68c8f92d9e26e766b738bd87b8a4c926d030
vantutran2k1 <vantu.tran2k1@gmail.com>
2025-01-29 14:56:21 +0700

    Initial commit

---
+++ src/main/java/com/vantutran2k1/consumer/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.vantutran2k1.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vantutran2k1/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantutran2k1.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
